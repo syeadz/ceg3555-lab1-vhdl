5b8786679a6994e4b972a3b7ec23692aebe747a3
Syed Yead Zaman <s.yead.zaman@gmail.com>
2023-10-06 20:25:13 -0400

    fixed run

--- run.sh
+++ run.sh
@@ -4,8 +4,12 @@ file=$1
 mkdir -p ./waveforms
 sh ./build.sh mux2to1 && sh ./test.sh mux2to1
 sh ./build.sh mux4to1 && sh ./test.sh mux4to1
+sh ./build.sh mux16to8
+sh ./build.sh mux32to8
 sh ./build.sh enardFF_2
 sh ./build.sh register8bit && sh ./test.sh register8bit
 sh ./build.sh shftleftreg8bit && sh ./test.sh shftleftreg8bit
 sh ./build.sh shftrightreg8bit && sh ./test.sh shftrightreg8bit
+sh ./build.sh datapath && sh ./test.sh datapath
+sh ./build.sh controlpath && sh ./test.sh controlpath
 sh ./build.sh lab1 && sh ./test.sh lab1
